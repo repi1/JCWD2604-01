92bbfc939d94958066b9390b4e4a0e7f0c172eed
repi1 <150763668+repi1@users.noreply.github.com>
2024-03-27 17:20:05 +0700

    Schema-Prisma
    update schema

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,39 @@
-import App from './app';
+/** @format */
 
-const main = () => {
-  // init db here
+import express, { Application, Response, Request, NextFunction } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { routes } from './routes';
+import cors from 'cors';
+import { config } from 'dotenv';
+config();
 
-  const app = new App();
-  app.start();
-};
+export const prisma = new PrismaClient();
 
-main();
+export const secretKey = String(process.env.secretKey);
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  '/public/events',
+  express.static(`${__dirname}/public/images/event_images`),
+);
+
+const PORT = process.env.PORT;
+
+//routes
+app.use('/users', routes.userRoutes);
+app.use('/products', routes.productRoutes);
+app.use('/transactions', routes.transactionRoutes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send({ message: error.message || 'internal server error' });
+}); //error handler
+
+app.use('*', (req: Request, res: Response, next: NextFunction) => {
+  res.status(404).send('page not found'); //page not found handler
+});
+
+app.listen(PORT, () => {
+  console.log('api is running on port', PORT);
+});
