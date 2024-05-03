02aa645a837a99053cc4c05dc668bd6fd43c6010
repi1 <150763668+repi1@users.noreply.github.com>
2024-05-03 09:22:11 +0700

    opencage api geolocation

--- apps/web/src/app/productPhotos/page.jsx
+++ apps/web/src/app/productPhotos/page.jsx
@@ -1,23 +1,64 @@
+'use client';
 import { axiosInstance, axiosInstanceSSR } from '@/axios/axios';
 import Link from '@mui/material/Link';
+import { useState, useEffect } from 'react';
 
-export const metadata = {
-  title: 'Product Detail',
-  description: 'tempat jualan :)',
-};
+// export const metadata = {
+//   title: 'Product Detail',
+//   description: 'tempat jualan :)',
+// };
 
-async function Page() {
+function Page() {
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const button = document.getElementById('detect-location-button');
+    if (button) {
+      button.addEventListener('click', handleDetectLocation);
+    }
+
+    return () => {
+      button.removeEventListener('click', handleDetectLocation);
+    };
+  }, []);
+
+  const handleDetectLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          setError(error.message);
+        },
+      );
+    } else {
+      setError('Browser not supported');
+    }
+  };
+
   return (
-    <>
-      <form>
-        <label for="fn">First Name</label>
-        <input type="text" name="fn" id="fn" /> <br />
-        <label for="ln">Last Name</label>
-        <input type="text" name="ln" id="ln" /> <br />
-        <input type="file" name="file" id="files" multiple /> <br />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+    <div>
+      <form>{/* your form fields */}</form>
+      <div>
+        <div id="location_details">
+          {location ? (
+            <p>
+              Your location: {location.latitude}, {location.longitude}
+            </p>
+          ) : (
+            <p>No location detected</p>
+          )}
+          {error ? <p>Error: {error}</p> : null}
+        </div>
+        <button id="detect-location-button">Detect your location</button>
+      </div>
+    </div>
   );
 }
+
 export default Page;
